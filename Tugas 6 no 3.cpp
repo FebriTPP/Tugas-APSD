648192d34c8349806df50acc080a1fab0d9a329a
FebriTPP <113004186+FebriTPP@users.noreply.github.com>
2022-10-29 00:02:17 +0700

    Create Tugas 6 no 3.cpp

---
+++ Tugas 6 no 3.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <ctype.h>
+#include <string.h>
+
+using namespace std;
+int main()
+{
+  char a1[20], a2[20];
+
+cout << "Masukan kata pertama = ";
+  cin >> a1;
+  cout << "Masukan kata kedua = ";
+  cin >> a2;
+
+strcat(a1,a2);
+  cout << "Menjadi = "<< a1;
+  getchar();
+  }
